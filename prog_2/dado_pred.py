36af326d778ee11f0899c2ebf059deedb54318ff
Federico <45139706+fededev01@users.noreply.github.com>
2020-05-10 10:36:29 +0200

    Update dado_pred.py

--- prog_2/dado_pred.py
+++ prog_2/dado_pred.py
@@ -5,12 +5,12 @@ value = random.randint(1, 6)
 print("Il numero estratto a caso è: ")
 print(value)
 if pred == value:
- print("Hai indovinato il nunero estratto a       caso")
+   print("Hai indovinato il nunero estratto a caso")
 elif pred >= 7:
- print("Il numero da te scelto è troppo grande")
- print("Scegli un numero tra l'1 e il 6")
+   print("Il numero da te scelto è troppo grande")
+   print("Scegli un numero tra l'1 e il 6")
 elif pred < 1:
- print("Il numero da te scelto è troppo piccolo")
- print("Scegli un numero tra l'1 e il 6")
+   print("Il numero da te scelto è troppo piccolo")
+   print("Scegli un numero tra l'1 e il 6")
 else:
- print("Non hai indovinato il numero estratto a caso")
+   print("Non hai indovinato il numero estratto a caso")
